6de243a790cc03628c5af647d0c91d28c1dbc587
zeinebbenali <benalii.zeineb@gmail.com>
2022-03-13 19:08:44 +0100

    update service

--- src/main/java/com/boot/demo/services/ArticleServiceImpl.java
+++ src/main/java/com/boot/demo/services/ArticleServiceImpl.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -19,16 +21,23 @@ public class ArticleServiceImpl implements ArticleService {
     }
 
     @Override
-    public Article showArticle(String name) {
-        log.info("Show an article {}", name);
-        Article acticle = articleRepository.save(Article.builder().id(1L).amount(BigDecimal.ONE).name(name).build());
+    public Article showArticle(Long id) {
+        log.info("Show an article {}", id);
+        Optional<Article> acticle = articleRepository.findById(id);
+        return acticle.isPresent() ? acticle.get() : null;
+    }
+
+    @Override
+    public List<Article> showAll() {
+        log.info("Show all articles");
+        List<Article> acticle = articleRepository.findAll();
         return acticle;
     }
 
     @Override
     public Boolean addArticle(Long id, String name) {
         log.info("Add a new article");
-        articleRepository.save(Article.builder().id(id).amount(BigDecimal.ONE).name(name).build());
+        articleRepository.save(Article.builder().amount(BigDecimal.ONE).name(name).build());
         return true;
     }
 
